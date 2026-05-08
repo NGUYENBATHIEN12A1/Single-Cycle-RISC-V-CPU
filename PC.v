2d7092d6ad68a4fa64da8fb48b91fec510e29c4d
MoKuBa <166572750+NGUYENBATHIEN12A1@users.noreply.github.com>
2026-05-08 10:00:44 +0700

    Update PC.v

--- PC.v
+++ PC.v
@@ -1,16 +1,10 @@
-module Program_Counter(
-    input clk,
-    input rst,
+module Program_Counter( 
+    input clk, rst,
     input [31:0] PC_in,
     output reg [31:0] PC_out
 );
-
-always @(posedge clk or posedge rst) begin
-    if (rst) begin
-        PC_out <= 32'b0;
-    end else begin
-        PC_out <= PC_in;
+    always @(posedge clk or posedge rst) begin 
+        if (rst) PC_out <= 32'b0;
+        else     PC_out <= PC_in;
     end
-end
-
 endmodule
