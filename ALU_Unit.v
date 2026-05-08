770f184711cb39708f4d56822340f97bfbacf46d
MoKuBa <166572750+NGUYENBATHIEN12A1@users.noreply.github.com>
2026-05-08 09:56:14 +0700

    Update ALU_Unit.v

--- ALU_Unit.v
+++ ALU_Unit.v
@@ -1,42 +1,17 @@
-//ALU_unit
-// ALU
 module ALU_unit (
     input  [31:0] A, B,
     input  [3:0]  Control_in,
     output reg [31:0] ALU_Result,
     output reg zero
 );
-
-always @(*) begin
-    case (Control_in)
-        4'b0000: begin // AND
-            ALU_Result = A & B;
-            zero = 0;
-        end
-
-        4'b0001: begin // OR
-            ALU_Result = A | B;
-            zero = 0;
-        end
-
-        4'b0010: begin // ADD
-            ALU_Result = A + B;
-            zero = 0;
-        end
-
-        4'b0110: begin // SUB
-            ALU_Result = A - B;
-            if (A == B)
-                zero = 1;
-            else
-                zero = 0;
-        end
-
-        default: begin
-            ALU_Result = 32'b0;
-            zero = 0;
-        end
-    endcase
-end
-
+    always @(*) begin 
+        case (Control_in)
+            4'b0000: ALU_Result = A & B;      // AND
+            4'b0001: ALU_Result = A | B;      // OR
+            4'b0010: ALU_Result = A + B;      // ADD
+            4'b0110: ALU_Result = A - B;      // SUB
+            default: ALU_Result = 32'b0;
+        endcase
+        zero = (ALU_Result == 32'b0) ? 1'b1 : 1'b0;
+    end
 endmodule
