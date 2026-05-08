9ea564d3a2a4fde63bd5eea17e6eb872bae7ea00
MoKuBa <166572750+NGUYENBATHIEN12A1@users.noreply.github.com>
2026-05-08 10:01:30 +0700

    Update Reg_File.v

--- Reg_File.v
+++ Reg_File.v
@@ -1,26 +1,20 @@
 module Reg_file(
-    input clk,
-    input rst,
-    input Reg_write,
+    input clk, rst, Reg_write,
     input [4:0] rs1, rs2, rd,
     input [31:0] write_data,
     output [31:0] read_data1, read_data2
 );
+    reg [31:0] Registers [31:0];
+    integer k;
 
-reg [31:0] Registers [31:0];
-integer k;
-
-always @(posedge clk or posedge rst) begin
-    if (rst) begin
-        for (k = 0; k < 32; k = k + 1) begin
-            Registers[k] <= 32'b0;
-        end
-    end else if (Reg_write && rd != 0) begin
-        Registers[rd] <= write_data;
+    always @(posedge clk or posedge rst) begin
+        if (rst) begin
+            for (k = 0; k < 32; k = k + 1) Registers[k] <= 32'b0;
+        end 
+        else if (Reg_write && rd != 5'b0) begin
+            Registers[rd] <= write_data;
+        end 
     end
-end
-
-assign read_data1 = Registers[rs1];
-assign read_data2 = Registers[rs2];
-
+    assign read_data1 = (rs1 == 5'b0) ? 32'b0 : Registers[rs1];
+    assign read_data2 = (rs2 == 5'b0) ? 32'b0 : Registers[rs2];
 endmodule
